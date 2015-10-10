079fe9fa80011d50110d7bcb11128391a31939c5
airbreak <954957945@qq.com>
2015-10-10 17:58:08 +0800

    服务器第一个版本

--- js/home.js
+++ js/home.js
@@ -4,13 +4,15 @@
 
 /*游戏对象*/
 var game = {
+    maxTimeStr:'00:00:10',
     $cWrapper: null,
     $gWrapper: null,
+    $gOverWrapper:null,
     timeInterval: null,
     questionsArr: [{ qId: 1, qDescription: '在ps中，ctrl+v是复制的快捷键对么？', answer: true }, { qId: 2, qDescription: '在ps中，ctrl+v是复制的快捷键对么？', answer: true },
-        { qId: 3, qDescription: '在ps中，ctrl+j是复制l图层的快捷键对么？', answer: true }, { qId: 4, qDescription: '在ps中，ctrl+b是取消选区的快捷键对么？', answer: false },
+        { qId: 3, qDescription: '在ps中，ctrl+j是复制图层的快捷键对么？', answer: true }, { qId: 4, qDescription: '在ps中，ctrl+b是取消选区的快捷键对么？', answer: false },
         { qId: 5, qDescription: '在ps中，p是快速蒙板的快捷键对么？', answer: true }, { qId: 6, qDescription: '在ps中，ctrl+i是反向选择的快捷键对么？', answer: true },
-        { qId: 7, qDescription: '在ps中，ctrl+t是标尺的快捷键对么？', answer: true }, { qId: 8, qDescription: '在ps中，b是的画笔的快捷键对么？', answer: true },
+        { qId: 7, qDescription: '在ps中，ctrl+t是标尺的快捷键对么？', answer: false }, { qId: 8, qDescription: '在ps中，b是的画笔的快捷键对么？', answer: true },
         { qId: 9, qDescription: '在ps中，ctrl+r是自由绽放的快捷键对么？', answer: false }, { qId: 10, qDescription: '在ps中，g是复制的快捷键对么？', answer: false }
     ],
     answersArr: [],  //答题情况
@@ -19,6 +21,7 @@ var game = {
     initialize: function () {
         this.$cWrapper = $('.contentWrapper');
         this.$gWrapper = $('.gameContentWrapper');
+        this.$gOverWrapper = $('.gameOverWrapper');
         this.eventsInit();
     },
 
@@ -35,9 +38,9 @@ var game = {
             if (index == 1) {
                 $btns.hide();
                 $('.gameRecords').show();
+            } else {
+                that.showGameContentWrapper();
             }
-            that.showGameContentWrapper();
-
         });
 
         /*关闭记录*/
@@ -59,6 +62,14 @@ var game = {
             });
             that.fillInQuestionToDom();   //随机出题
         });
+
+        /*分享和 再来一盘*/
+        that.$gOverWrapper.on('click', '.oneMoreTime', function () {
+            var index = $(this).index();
+            if (index == 1) {
+                that.restarGame();
+            }
+        });
     },
 
     /*
@@ -67,10 +78,11 @@ var game = {
     */
     showGameContentWrapper: function (imgSrc) {
         this.$cWrapper.hide();
-        $('.gameContentWrapper').addClass('gameContentWrapperShow');
+        this.swapClass($('.gameContentWrapper'), 'gameContentWrapperHide', 'gameContentWrapperShow');
         this.getQuestionsFromService(function (data) {
             this.questionsArr = data;
             this.fillInQuestionToDom();   //随机出题
+            this.initTimeDetailInfo();//初始化时间
             this.setTimeOutInfo();
         });
     },
@@ -114,6 +126,11 @@ var game = {
         this.timeInterval = window.setInterval(function () {
             that.updateTimeShowInfo.call(that, $timeLabel);
         }, 1000);
+    },
+
+    /*初始化最大时间*/
+    initTimeDetailInfo: function () {
+        this.$gWrapper.find('.timeDetailInfo').text(this.maxTimeStr);
     },
 
     /*更新显示出来的时间*/
@@ -170,10 +187,11 @@ var game = {
                 i = 0;
             }
         }
-        that.calculateScroes(i);
-
-        $('.gameOverWrapper').show().siblings().hide();
-       console.log(that.totalScores);
+        if (i != 0) {
+            that.calculateScroes(i);
+        }
+        this.showGameResultPanel();  //显示本次游戏结果
+        this.commitResultToService();//将游戏结果传到服务器
     },
 
     /*
@@ -187,6 +205,49 @@ var game = {
     */
     calculateScroes: function (i) {
         this.totalScores += Math.pow(24, i);
+    },
+
+    /*将游戏结果传到服务器*/
+    commitResultToService: function () {
+
+    },
+
+    /*显示本次游戏和排行*/
+    showGameResultPanel: function () {
+        this.$gOverWrapper.show();
+        $('#totalScores').text(this.totalScores);
+        this.swapClass(this.$gWrapper, 'gameContentWrapperShow', 'gameContentWrapperHide');
+        this.swapClass(this.$gOverWrapper.find('.gameResultPanel'), 'gameResultPanelHide', 'gameResultPanelShow');
+        this.swapClass(this.$gOverWrapper.find('.gameRankingListPanel'), 'gameRankingListPanelHide', 'gameRankingListPanelShow');
+    },
+
+    /*
+    *重新开始游戏
+    *需要执行以下过程：
+    *1.显示首页，隐藏当前的页面。
+    *2.页面效果 之前出现的效果类要去除包括：
+    *   游戏主面板的 滑入效果，结果板的 掉下效果
+    *3.清除上次游戏信息 计时的数字改成30s
+    *
+    */
+    restarGame: function () {
+        this.clearGameInfo();  //清除上次游戏信息
+        this.$cWrapper.delay(500).show(0);
+        this.swapClass(this.$gWrapper, 'gameContentWrapperShow', 'gameContentWrapperHide');
+        this.swapClass(this.$gOverWrapper.find('.gameResultPanel'), 'gameResultPanelShow', 'gameResultPanelHide');
+        this.swapClass(this.$gOverWrapper.find('.gameRankingListPanel'), 'gameRankingListPanelShow', 'gameRankingListPanelHide');
+    },
+
+    /*清除上次游戏信息*/
+    clearGameInfo: function () {
+        $('#totalScores').text('');
+        this.totalScores = 0;
+        this.answersArr = [];
+    },
+
+    /*样式转换*/
+    swapClass: function ($target, oClass, nClass) {
+        $target.removeClass(oClass).addClass(nClass);
     },
 
     OBJECT_NAME: 'game'
